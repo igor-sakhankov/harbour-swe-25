dbc21dd30a80e1fea40d44390f2231385ae35d69
Igor <igor.sakhankov@glovoapp.com>
2025-02-28 09:43:23 +0100

    formatting

---
+++ springboot/src/main/java/com/harbour/springboot/MyVariable.java
@@ -0,0 +1,24 @@
+package com.harbour.springboot;
+
+public class MyVariable {
+
+    public void myMethod() {
+        System.out.println("Hello world!");
+    }
+
+    private void myPrivateMethod() {
+        System.out.println("Hello world!");
+    }
+
+    private void myPrivateMethod3() {
+        System.out.println("Hello world!");
+    }
+
+    private void myPrivateMethod4() {
+        System.out.println("Hello world!");
+    }
+
+    private void myPrivateMethod2() {
+        System.out.println("Hello world!");
+    }
+}
--- springboot/src/main/java/com/harbour/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/harbour/springboot/SpringbootApplication.java
@@ -3,10 +3,14 @@ package com.harbour.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
+
+        List.of().forEach(System.out::println);
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
